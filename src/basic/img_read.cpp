4f968e89d29bbcb38de163f49d9042e6a36482c8
shinjitorikai <55309564+shinjitorikai@users.noreply.github.com>
2020-05-17 13:43:44 +0900

    Add basic/img_read.cpp

--- src/basic/img_read.cpp
+++ src/basic/img_read.cpp
@@ -7,26 +7,29 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-#if 0
-
-int main()
+static void read_image()
 {
-	// 幅320px、高さ240pxで赤色の画像データを生成
-	cv::Mat redImg(cv::Size(320, 240), CV_8UC3, cv::Scalar(0, 0, 255));
-	
-	// 画像表示用のウィンドウを生成
-	cv::namedWindow("red", cv::WINDOW_AUTOSIZE);
+	cv::Mat img;
 
-	// ウィンドウに画像を表示
-	cv::imshow("red", redImg);
+	// 画像の読み込み
+	img = cv::imread("test.png");
+
+	// 画像の表示
+	cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
+	cv::imshow("image", img);
 
 	// キー入力を待機
 	cv::waitKey(0);
 
 	// 作成したウィンドウを全て破棄
 	cv::destroyAllWindows();
+}
+
+#if 1
+int main()
+{
+	read_image();
 	
 	return 0;
 }
-
 #endif
